dc1856d32cf4335b7e021bdddb45c33bf988ba29
Mihir Pathak <mihirpathak267@gmail.com>
2023-03-09 18:26:39 +0530

    Create a react app.
    Make the basic layout for the keeper app using components.

---
+++ src/components/Note.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Note(){
+    return <div className="note">
+        <h1>Default Heading</h1>
+        <p>Default Content</p>
+    </div>
+}
+
+export default Note;
